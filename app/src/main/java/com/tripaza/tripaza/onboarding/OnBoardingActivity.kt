75397d19ab222b2bc6357cc30b3269224805a514
Anla Rijabani Ramsus <ramsusanla3009@gmail.com>
2022-05-28 13:39:46 +0700

    onboardingactivity

--- app/src/main/java/com/tripaza/tripaza/onboarding/OnBoardingActivity.kt
+++ app/src/main/java/com/tripaza/tripaza/onboarding/OnBoardingActivity.kt
@@ -1,11 +1,23 @@
 package com.tripaza.tripaza.onboarding
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tripaza.tripaza.databinding.ActivityOnBoardingBinding
+import com.tripaza.tripaza.ui.registration.MainActivity
 
 class OnBoardingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOnBoardingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_on_boarding)
+        binding = ActivityOnBoardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            val intent = Intent(this@OnBoardingActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
