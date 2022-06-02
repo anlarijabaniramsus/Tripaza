6afa3b4ed8dde811c2d45a771ef4044cd763ba46
muhammadaufa07 <62533996+muhammadaufa07@users.noreply.github.com>
2022-06-02 23:17:35 +0700

    maps added, recyclerview added

--- app/src/main/java/com/tripaza/tripaza/ui/navigation/ui/maps/MapsFragment.kt
+++ app/src/main/java/com/tripaza/tripaza/ui/navigation/ui/maps/MapsFragment.kt
@@ -1,43 +1,67 @@
 package com.tripaza.tripaza.ui.navigation.ui.maps
 
+import androidx.fragment.app.Fragment
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.tripaza.tripaza.databinding.FragmentNotificationsBinding
+import androidx.recyclerview.widget.LinearLayoutManager
 
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.tripaza.tripaza.R
+import com.tripaza.tripaza.databinding.FragmentHomeBinding
+import com.tripaza.tripaza.databinding.FragmentMapsBinding
+import com.tripaza.tripaza.ui.navigation.ui.home.HomeViewModel
 
 class MapsFragment : Fragment() {
-
-    private var _binding: FragmentNotificationsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private lateinit var mapsViewModel: MapsViewModel
+    private lateinit var searchResultListAdapter: SearchResultListAdapter
+    private var _binding: FragmentMapsBinding? = null
     private val binding get() = _binding!!
+    private val callback = OnMapReadyCallback { googleMap ->
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val mapsViewModel =
-            ViewModelProvider(this).get(MapsViewModel::class.java)
-
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textNotifications
-        mapsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+    ): View? {
+        _binding = FragmentMapsBinding.inflate(inflater, container, false)
+        mapsViewModel = ViewModelProvider(this).get(MapsViewModel::class.java)
+        searchResultListAdapter = SearchResultListAdapter()
+        return binding.root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+        
+        
+        mapsViewModel.placeList.observe(viewLifecycleOwner){
+            searchResultListAdapter.setPlaceList(it)
+        }
+        binding.rvSearchResult.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvSearchResult.adapter = searchResultListAdapter
     }
 }
--- app/src/main/java/com/tripaza/tripaza/ui/navigation/ui/maps/MapsViewModel.kt
+++ app/src/main/java/com/tripaza/tripaza/ui/navigation/ui/maps/MapsViewModel.kt
@@ -3,11 +3,27 @@ package com.tripaza.tripaza.ui.navigation.ui.maps
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.tripaza.tripaza.databases.dataobject.Place
+import kotlin.math.abs
+import kotlin.random.Random
 
 class MapsViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "Maps Fragment"
+    private val _placeList = MutableLiveData<ArrayList<Place>>().apply {
+        value = generatePlaceList("VeryLongPlaceString")
     }
-    val text: LiveData<String> = _text
+
+    val placeList: LiveData<ArrayList<Place>> = _placeList
+    
+    private fun generatePlaceList(identifier: String): ArrayList<Place>{
+        val data = arrayListOf<Place>()
+        val character = arrayListOf<String>("A", "B", "C", "D")
+        val div = character.size
+        var ptr = 0
+        data.add(Place("OFFSET", "OFFSET"))
+        for (i in 1..10_000){
+            val rating = (abs(Random.nextInt()) %5)+1
+            data.add( Place("${identifier} ${character[i%div]}${ptr}", "My ${identifier} ${character[i%div]}${ptr}", "My ${identifier} Location ${character[i%div]}${ptr}", "My ${identifier} description ${character[i%div]}${ptr++}", rating) )
+        }
+        return data
+    }
 }
