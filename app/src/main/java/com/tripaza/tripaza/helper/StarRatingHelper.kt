021e3003faa3ea7fbeeba1eceaad84fda9cefb5a
muhammadaufa07 <62533996+muhammadaufa07@users.noreply.github.com>
2022-06-02 23:47:03 +0700

    StarRatingHelper.kt bug fix

--- app/src/main/java/com/tripaza/tripaza/helper/StarRatingHelper.kt
+++ app/src/main/java/com/tripaza/tripaza/helper/StarRatingHelper.kt
@@ -5,10 +5,18 @@ import com.tripaza.tripaza.databinding.StarRatingBinding
 
 object StarRatingHelper {
     fun setStarRating(item: StarRatingBinding, rating: Int){
-        if (rating >= 1) item.star1.setImageResource(R.drawable.ic_baseline_star_rate_24_gold)
-        if (rating >= 2) item.star2.setImageResource(R.drawable.ic_baseline_star_rate_24_gold)
-        if (rating >= 3) item.star3.setImageResource(R.drawable.ic_baseline_star_rate_24_gold)
-        if (rating >= 4) item.star4.setImageResource(R.drawable.ic_baseline_star_rate_24_gold)
-        if (rating >= 5) item.star5.setImageResource(R.drawable.ic_baseline_star_rate_24_gold)
+        val ratingStar = arrayOf(
+            item.star1,
+            item.star2,
+            item.star3,
+            item.star4,
+            item.star5
+        )
+        for (i in ratingStar.indices){
+            if ( i+1 <= rating )
+                ratingStar[i].setImageResource(R.drawable.ic_baseline_star_rate_24_gold)
+            else
+                ratingStar[i].setImageResource(R.drawable.ic_baseline_star_rate_24)
+        }
     }
 }
