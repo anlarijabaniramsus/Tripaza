c0b582d562d2f8a4bde074303730dd8a3c64cfae
muhammadaufa07 <62533996+muhammadaufa07@users.noreply.github.com>
2022-06-05 21:22:38 +0700

    update DUMMY_IMAGE_FEATURED to non-animated images

--- app/src/main/java/com/tripaza/tripaza/helper/Constants.kt
+++ app/src/main/java/com/tripaza/tripaza/helper/Constants.kt
@@ -9,7 +9,8 @@ object Constants {
     // Dummy Images urls
     const val DUMMY_IMAGE_PLACE = "https://images.hdqwalls.com/download/small-memory-8k-2a-1920x1080.jpg"
     const val DUMMY_IMAGE_FOOD = "https://images.pling.com/img/00/00/63/48/00/1646778/39857213912be2a47096d2983bad03a3756de42ad83b245ca7e5f3e75f62d0f910df.jpg"
-    const val DUMMY_IMAGE_FEATURED = "https://cdnb.artstation.com/p/assets/images/images/024/538/827/original/pixel-jeff-clipa-s.gif?1582740711"
+//    const val DUMMY_IMAGE_FEATURED = "https://cdnb.artstation.com/p/assets/images/images/024/538/827/original/pixel-jeff-clipa-s.gif?1582740711"
+    const val DUMMY_IMAGE_FEATURED = "https://media.istockphoto.com/photos/retro-futuristic-city-flythrough-background-80s-scifi-landscape-in-picture-id1248619427?b=1&k=20&m=1248619427&s=170667a&w=0&h=svQPXt8hCva0D5xKx4l7LHW9v9zjZfBJwfWWWUMvFWw="
     const val DUMMY_IMAGE_PROFILE = "https://wallpaperaccess.com/full/4958080.jpg"
     
 }
